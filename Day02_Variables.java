9597c82c13c679a8b92915797fc38726168bffb6
Yogamalar Ananthan <m-11516684@moe-dl.edu.my>
2026-07-25 14:34:35 +0800

    Add Day02_Variables class with main method

---
+++ Day02_Variables.java
@@ -0,0 +1,15 @@
+public class Day02_Variables {
+
+    public static void main(String[] args) {
+
+        String name = "Yoga";
+        int age = 15;
+        String dream = "Cloud Security Specialist";
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(dream);
+
+    }
+
+}
