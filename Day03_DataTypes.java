4fe3b90435cc81f1207b9863a419037956522611
Yogamalar Ananthan <yogamalarkanniga@gmail.com>
2026-07-26 19:23:10 +0800

    Add Day03_DataTypes class with basic data types

---
+++ Day03_DataTypes.java
@@ -0,0 +1,19 @@
+public class Day03_DataTypes {
+
+    public static void main(String[] args) {
+
+        String name = "Yogamalar";
+        int age = 15;
+        double height = 155.5;
+        boolean likesJava = true;
+        char grade = 'A';
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(likesJava);
+        System.out.println(grade);
+
+    }
+
+}
